58b392059c3d3e4da212065420bb8d97e9a2fad3
William Mian <william_mian@yahoo.com.br>
2024-11-22 09:06:23 -0300

    CorrelationID

--- microservicos-evolution2/ecommerce/service-fraud-detector/src/main/java/br/com/wm/ecommerce/FraudDetectorService.java
+++ microservicos-evolution2/ecommerce/service-fraud-detector/src/main/java/br/com/wm/ecommerce/FraudDetectorService.java
@@ -21,29 +21,34 @@ public class FraudDetectorService {
 
     private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
 
-    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException {
+    private void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
         System.out.println("------------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
         System.out.println(record.value());
         System.out.println(record.partition());
         System.out.println(record.offset());
+
+        var message = record.value();
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             // ignoring
             e.printStackTrace();
         }
-        var order = record.value();
+        var order = message.getPayload();
         if(isFraud(order)) {
             // pretending that the fraud happens when the amount is >= 4500
             System.out.println("Order is a fraud!!!!!" + order);
-            orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getEmail(), order);
+            orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getEmail(),
+                    message.getId().continueWith(FraudDetectorService.class.getSimpleName()),
+                    order);
         } else {
             System.out.println("Approved: " + order);
-            orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getEmail(), order);
+            orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getEmail(),
+                    message.getId().continueWith(FraudDetectorService.class.getSimpleName()),
+                    order);
         }
-
     }
 
     private boolean isFraud(Order order) {
