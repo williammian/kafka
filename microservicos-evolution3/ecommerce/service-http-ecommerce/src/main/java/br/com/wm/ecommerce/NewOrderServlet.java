1826911bc747c29bd53ed0d53ec0a63b8e46577e
William Mian <william_mian@yahoo.com.br>
2024-11-22 15:02:07 -0300

    microservices de email e fast delagate real

--- microservicos-evolution3/ecommerce/service-http-ecommerce/src/main/java/br/com/wm/ecommerce/NewOrderServlet.java
+++ microservicos-evolution3/ecommerce/service-http-ecommerce/src/main/java/br/com/wm/ecommerce/NewOrderServlet.java
@@ -14,13 +14,11 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderServlet extends HttpServlet {
 
     private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
-    private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
 
     @Override
     public void destroy() {
         super.destroy();
         orderDispatcher.close();
-        emailDispatcher.close();
     }
 
     @Override
@@ -38,11 +36,6 @@ public class NewOrderServlet extends HttpServlet {
             orderDispatcher.send("ECOMMERCE_NEW_ORDER", email,
                     new CorrelationId(NewOrderServlet.class.getSimpleName()),
                     order);
-
-            var emailCode = "Thank you for your order! We are processing your order!";
-            emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email,
-                    new CorrelationId(NewOrderServlet.class.getSimpleName()),
-                    emailCode);
 
             System.out.println("New order sent successfully.");
             resp.setStatus(HttpServletResponse.SC_OK);
--- microservicos-evolution3/ecommerce/service-new-order/src/main/java/br/com/wm/ecommerce/NewOrderMain.java
+++ microservicos-evolution3/ecommerce/service-new-order/src/main/java/br/com/wm/ecommerce/NewOrderMain.java
@@ -10,23 +10,17 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         try (var orderDispatcher = new KafkaDispatcher<Order>()) {
-            try (var emailDispatcher = new KafkaDispatcher<String>()) {
-                var email = Math.random() + "@email.com";
-                for (var i = 0; i < 10; i++) {
+            var email = Math.random() + "@email.com";
+            for (var i = 0; i < 10; i++) {
 
-                    var orderId = UUID.randomUUID().toString();
-                    var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
+                var orderId = UUID.randomUUID().toString();
+                var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
 
-                    var id = new CorrelationId(NewOrderMain.class.getSimpleName());
+                var id = new CorrelationId(NewOrderMain.class.getSimpleName());
 
-                    var order = new Order(orderId, amount, email);
-                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", email,
-                            id, order);
-
-                    var emailCode = "Thank you for your order! We are processing your order!";
-                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email,
-                            id, emailCode);
-                }
+                var order = new Order(orderId, amount, email);
+                orderDispatcher.send("ECOMMERCE_NEW_ORDER", email,
+                        id, order);
             }
         }
     }
